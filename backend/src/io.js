3545430871024b32cb621f7f99e99bdf6eea7873
= <=>
2024-11-12 15:03:41 +0530

    adding ice and sdp configurations

--- backend/src/io.js
+++ backend/src/io.js
@@ -11,13 +11,33 @@ export default function setupSocketIO(server) {
         }
     });
     io.on("connection", (socket) => {
-        socket.on("register-code", async (code, callback) => {
+        console.log("a user is connected:", socket.id);
+        socket.on("get-code", (callback) => {
             asyncHandler(async () => {
+                let code = "";
+                do {
+                    code = `${Math.floor(100000 + Math.random() * 900000)}`;
+                } while (await !AccessCode.findOne({ accessCode: code }));
                 await AccessCode.create({ accessCode: code, socketId: socket.id });
-                if (callback) callback(apiResponse(200, "Access code mapped successfully"));
+                if (callback) callback(apiResponse(200, "Access code mapped successfully", { code }));
             })();
         });
+
+        socket.on("check-code", (code, callback) => {
+            asyncHandler(async () => {
+                const accessCode = await AccessCode.findOne({ accessCode: code });
+                if (accessCode) return callback(apiResponse(200, "Socket Id found", { socketId: accessCode.socketId }));
+                callback(apiResponse(404, "No Socked Id found"));
+            })();
+        });
+
+        socket.on("getReceiverSocketId", (ids) => {
+            console.log("ids received from receiver:", ids);
+            io.to(ids.senderSockedId).emit("getReceiverSocketId", ids.receiverSocketId);
+        });
+        
         socket.on("disconnect", async () => {
+            console.log("a user is disconnected:", socket.id);
             await AccessCode.findOneAndDelete({ socketId: socket.id });
         });
     });
